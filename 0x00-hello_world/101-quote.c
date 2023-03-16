1d08bc4addcbe6736be692d12bd28a45cf371297
edgaryungo <eddy8471@gmail.com>
2023-03-16 06:18:50 -0700

    final commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <sys/syscall.h>
 /**
  * main - entry
  * Return: Always 1(success)
@@ -7,6 +8,6 @@ int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str, 59);
+	syscall(2, str, 59);
 	return (1);
 }
