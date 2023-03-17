19cfea3f9d43fc24dd26dc8dccb0142737a9b183
edgaryungo <eddy8471@gmail.com>
2023-03-17 04:21:00 -0700

    c variables

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	/*char alpt[26] = "abcdefghijklmnopqrstuvwxyz";*/
+	char alpt[26] = "abcdefghijklmnopqrstuvwxyz";
 	/*reverse(alpt);*/
 	int i;
 
 	for (i = 26; i > 0; i--)
 	{
-		putchar(i);
+		putchar(alpt[i]);
 	}
 	putchar('\n');
 	return (0);
