d9c90610ce1f8571a71700204e2fa5d1e44f1268
edgaryungo <eddy8471@gmail.com>
2023-03-23 07:01:53 -0700

    betty fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,8 @@ void print_most_numbers(void)
 
 	for (a = 48; a < 58; a++)
 	{
-		if (a == 50 && a == 52)
-		{
-			break;
-		}
-		_putchar(a);
+		if (a != 50 && a != 52)
+			_putchar(a);
 	}
 	_putchar('\n');
 }
