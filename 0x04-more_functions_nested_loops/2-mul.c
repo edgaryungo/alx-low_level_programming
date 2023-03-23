11dc19817f12e09f87d441cbaea2ca729320d7fd
edgaryungo <eddy8471@gmail.com>
2023-03-23 06:25:56 -0700

    first part commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * mul - multiply two inputs
+ *
+ * Return: 0 on success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 t0 9 exclude 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		if ( a == 50 && a == 52)
+		{
+			break;
+		}
+		_putchar(a);
+	}
+	_putchar('\n');
+}
